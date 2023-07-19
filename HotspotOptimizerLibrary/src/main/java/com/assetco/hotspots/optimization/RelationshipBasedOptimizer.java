2df0f80b37edd5b92a57d19aa72a954a2b8c4c6c
Zoltán Zemko <zemko_z@hotmail.com>
2023-07-19 23:01:15 +0200

    Add tests to BugsTest for test correct behaviour of getting Showcase after optimize search results. Fix the bug in RelationshipBasedOptimizer.

--- HotspotOptimizerLibrary/src/main/java/com/assetco/hotspots/optimization/RelationshipBasedOptimizer.java
+++ HotspotOptimizerLibrary/src/main/java/com/assetco/hotspots/optimization/RelationshipBasedOptimizer.java
@@ -10,74 +10,114 @@ import java.util.*;
 import static com.assetco.search.results.AssetVendorRelationshipLevel.*;
 import static com.assetco.search.results.HotspotKey.*;
 
+// This code manages filling the showcase if it's not already set
+// it make sure the first partner-lvl vendor with enough assets on the page gets the showcase
+//
+// From Jamie's reqs:
+//   1. If a Partner-level vendor has at least three (3) assets in the result set, that partner's assets shall own the showcase
+//   2. If two (2) Partner-level vendors meet the criteria to own the showcase, the first vendor to meet the criteria shall own the showcase
+//   3. If a Partner-level has more than five (5) showcase assets, additional assets shall be treated as Top Picks
+//
+// -johnw
+// 1/3/07
+
+/**
+ * Assigns assets to the showcase hotspot group based on their vendor status.
+ */
 class RelationshipBasedOptimizer {
+    private static final int MAX_ASSETS_IN_SHOWCASE = 5;
+    private static final int ASSETS_COUNTS_TO_OWN_SHOWCASE = 3;
+
     public void optimize(SearchResults searchResults) {
-        Iterator<Asset> iterator = searchResults.getFound().iterator();
+        // don't affect a showcase built by an earlier rule
         var showcaseFull = searchResults.getHotspot(Showcase).getMembers().size() > 0;
-        List<Asset> showcaseAssets = new ArrayList<>();
-        // We need to "remember" the assets per partner
-        var showcaseAssetsByPartner = new HashMap<AssetVendor, List<Asset>>();
         var partnerAssets = new ArrayList<Asset>();
         var goldAssets = new ArrayList<Asset>();
         var silverAssets = new ArrayList<Asset>();
 
-        while (iterator.hasNext()) {
-            Asset asset = iterator.next();
-            if (asset.getVendor().getRelationshipLevel() == Gold)
+        for (Asset asset : searchResults.getFound()) {
+            // HACK! trap gold and silver assets for use later
+            if (asset.getVendor().getRelationshipLevel() == Gold) {
                 goldAssets.add(asset);
-            else if (asset.getVendor().getRelationshipLevel() == Silver)
+            } else if (asset.getVendor().getRelationshipLevel() == Silver) {
                 silverAssets.add(asset);
-
-            if (asset.getVendor().getRelationshipLevel() != Partner)
-                continue;
-
-            partnerAssets.add(asset);
-
-            if (showcaseAssets.size() >= 5) {
-                if (Objects.equals(showcaseAssets.get(0).getVendor(), asset.getVendor()))
-                    searchResults.getHotspot(TopPicks).addMember(asset);
-            } else {
-                if (showcaseAssets.size() != 0)
-                    if (!Objects.equals(showcaseAssets.get(0).getVendor(), asset.getVendor()))
-                        if (showcaseAssets.size() < 3)
-                            // instead of clearing the current showcase, just switch to one for the new target partner
-                            showcaseAssets = getShowcaseAssetsForPartner(showcaseAssetsByPartner, asset.getVendor());
-
-                if (showcaseAssets.size() == 0 || Objects.equals(showcaseAssets.get(0).getVendor(), asset.getVendor()))
-                    showcaseAssets.add(asset);
+            } else if (asset.getVendor().getRelationshipLevel() == Partner) {
+                // remember this partner asset
+                partnerAssets.add(asset);
             }
         }
 
+        var showcaseAssets = createShowcaseFromPartnerAssets(partnerAssets, searchResults);
+
+        // [DBV], 4/14/2014:
+        // need added this here even though it's not about this rules
+        // frm Jamie,
+        // 1. All partner assets should be eligible for high-value slots in the main grid.
+        // 2. All partner assets should be eligible to appear in the fold.
+
+        // todo - this does not belong here!!!
         var highValueHotspot = searchResults.getHotspot(HighValue);
         for (var asset : partnerAssets)
             if (!highValueHotspot.getMembers().contains(asset))
                 highValueHotspot.addMember(asset);
 
+        // TODO - this needs to be moved to something that only manages the fold
         for (var asset : partnerAssets)
             searchResults.getHotspot(Fold).addMember(asset);
 
-        if (!showcaseFull && showcaseAssets.size() >= 3) {
+        // only copy showcase assets into hotspot if there are enough for a partner to claim the showcase
+        if (!showcaseFull && showcaseAssets.size() >= ASSETS_COUNTS_TO_OWN_SHOWCASE) {
             Hotspot showcaseHotspot = searchResults.getHotspot(Showcase);
             for (Asset asset : showcaseAssets)
                 showcaseHotspot.addMember(asset);
         }
 
+        // acw-14339: gold assets should be in high value hotspots if there are no partner assets in search
         for (var asset : goldAssets)
             if (!highValueHotspot.getMembers().contains(asset))
                 highValueHotspot.addMember(asset);
 
+        // acw-14341: gold assets should appear in fold box when appropriate
         for (var asset : goldAssets)
             searchResults.getHotspot(Fold).addMember(asset);
 
+        // LOL acw-14511: gold assets should appear in fold box when appropriate
         for (var asset : silverAssets)
             searchResults.getHotspot(Fold).addMember(asset);
     }
 
-    private List<Asset> getShowcaseAssetsForPartner(Map<AssetVendor, List<Asset>> map, AssetVendor vendor) {
-        if (map.containsKey(vendor))
-            return map.get(vendor);
-        var result = new ArrayList<Asset>();
-        map.put(vendor, result);
-        return result;
+    private List<Asset> createShowcaseFromPartnerAssets(ArrayList<Asset> partnerAssets, SearchResults searchResults) {
+        List<Asset> showcaseAssets = new ArrayList<>();
+        var assetsByVendorMap = new HashMap<AssetVendor, List<Asset>>();
+
+        for (Asset asset : partnerAssets) {
+            List<Asset> currentAssetList = getAssetsListFromAssetsVendorMap(asset, assetsByVendorMap);
+
+            if (showcaseAssets.size() >= ASSETS_COUNTS_TO_OWN_SHOWCASE) {
+
+                if (showcaseAssets == currentAssetList) {
+                    if (showcaseAssets.size() >= MAX_ASSETS_IN_SHOWCASE) {
+                        searchResults.getHotspot(TopPicks).addMember(asset);
+                    } else {
+                        showcaseAssets.add(asset);
+                    }
+                }
+            } else {
+                showcaseAssets = currentAssetList;
+                showcaseAssets.add(asset);
+            }
+        }
+
+        if (showcaseAssets.size() < ASSETS_COUNTS_TO_OWN_SHOWCASE) {
+            return Collections.emptyList();
+        }
+        return showcaseAssets;
+    }
+
+    private static List<Asset> getAssetsListFromAssetsVendorMap(Asset asset, Map<AssetVendor, List<Asset>> assetsByVendorMap) {
+        if (!assetsByVendorMap.containsKey(asset.getVendor())) {
+            assetsByVendorMap.put(asset.getVendor(), new ArrayList<>());
+        }
+        return assetsByVendorMap.get(asset.getVendor());
     }
 }
--- HotspotOptimizerLibrary/src/test/java/com/assetco/hotspots/optimization/BugsTest.java
+++ HotspotOptimizerLibrary/src/test/java/com/assetco/hotspots/optimization/BugsTest.java
@@ -4,40 +4,76 @@ import com.assetco.search.results.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.List;
 
 import static com.assetco.hotspots.optimization.Any.*;
 import static com.assetco.search.results.HotspotKey.Showcase;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class BugsTest {
-    private SearchResultHotspotOptimizer searchResultHotspotOptimizer;
+    private SearchResultHotspotOptimizer sut;
     private SearchResults searchResults;
+    private AssetVendor firstPartnerVendor;
+    private AssetVendor secondPartnerVendor;
 
     @BeforeEach
     void setUp() {
+        firstPartnerVendor = makeVendor(AssetVendorRelationshipLevel.Partner);
+        secondPartnerVendor = makeVendor(AssetVendorRelationshipLevel.Partner);
+
         searchResults = new SearchResults();
-        searchResultHotspotOptimizer = new SearchResultHotspotOptimizer();
+        sut = new SearchResultHotspotOptimizer();
     }
 
     @Test
-    void precedingPartnerWithLongTrailingAssetsDoesNotWin() {
-        AssetVendor partnerVendor = makeVendor(AssetVendorRelationshipLevel.Partner);
-        Asset missingAsset = givenAssetInResultsWithVendor(partnerVendor);
-
-        AssetVendor anotherPartnerVendor = makeVendor(AssetVendorRelationshipLevel.Partner);
-        givenAssetInResultsWithVendor(anotherPartnerVendor);
+    void firstPartnerWithLongTrailingAssetsWin() {
+        Asset firstAssetOfPartnerVendor = givenAssetInResultsWithVendor(firstPartnerVendor);
+        Asset anotherPartnerAsset = givenAssetInResultsWithVendor(secondPartnerVendor);
 
         List<Asset> expected = List.of(
-                givenAssetInResultsWithVendor(partnerVendor),
-                givenAssetInResultsWithVendor(partnerVendor),
-                givenAssetInResultsWithVendor(partnerVendor),
-                givenAssetInResultsWithVendor(partnerVendor)
+                firstAssetOfPartnerVendor,
+                givenAssetInResultsWithVendor(firstPartnerVendor),
+                givenAssetInResultsWithVendor(firstPartnerVendor),
+                givenAssetInResultsWithVendor(firstPartnerVendor),
+                givenAssetInResultsWithVendor(firstPartnerVendor)
         );
-        
+
         whenOptimize();
 
-        thenHotspotDoesNotHave(Showcase, missingAsset);
+        thenHotspotDoesNotHave(Showcase, anotherPartnerAsset);
+        thenHotspotHasExactly(Showcase, expected);
+    }
+
+    @Test
+    void noOneOwnsShowcase() {
+        givenAssetInResultsWithVendor(firstPartnerVendor);
+        givenAssetInResultsWithVendor(secondPartnerVendor);
+        givenAssetInResultsWithVendor(secondPartnerVendor);
+        givenAssetInResultsWithVendor(firstPartnerVendor);
+
+        List<Asset> expected = Collections.emptyList();
+
+        whenOptimize();
+
+        thenHotspotHasExactly(Showcase, expected);
+    }
+
+    @Test
+    void secondPartnerWinsOnlyWithThreeAssets() {
+        var firstPartnerAsset1 = givenAssetInResultsWithVendor(firstPartnerVendor);
+        var secondPartnerAssert1 = givenAssetInResultsWithVendor(secondPartnerVendor);
+        var secondPartnerAssert2 = givenAssetInResultsWithVendor(secondPartnerVendor);
+        var firstPartnerAsset2 = givenAssetInResultsWithVendor(firstPartnerVendor);
+        var secondPartnerAssert3 = givenAssetInResultsWithVendor(secondPartnerVendor);
+        var firstPartnerAsset3 = givenAssetInResultsWithVendor(firstPartnerVendor);
+        var firstPartnerAsset4 = givenAssetInResultsWithVendor(firstPartnerVendor);
+        var expected = List.of(secondPartnerAssert1, secondPartnerAssert2, secondPartnerAssert3);
+
+        whenOptimize();
+
+        thenHotspotDoesNotHave(Showcase, firstPartnerAsset1, firstPartnerAsset2, firstPartnerAsset3, firstPartnerAsset4);
         thenHotspotHasExactly(Showcase, expected);
     }
 
@@ -53,12 +89,14 @@ class BugsTest {
     }
 
     private void whenOptimize() {
-        searchResultHotspotOptimizer.optimize(searchResults);
+        sut.optimize(searchResults);
     }
 
-    private void thenHotspotDoesNotHave(HotspotKey hotspotKey, Asset forbiddenAsset) {
+    private void thenHotspotDoesNotHave(HotspotKey hotspotKey, Asset...forbiddenAssets) {
         Hotspot hotspot = searchResults.getHotspot(hotspotKey);
-        assertFalse(hotspot.getMembers().contains(forbiddenAsset));
+        for (var forbiddenAsset : forbiddenAssets) {
+            assertFalse(hotspot.getMembers().contains(forbiddenAsset));
+        }
     }
 
     private void thenHotspotHasExactly(HotspotKey hotspotKey, List<Asset> expectedAssetList) {
